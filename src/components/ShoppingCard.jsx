e8c11e60856f304dcb1923b91b57cab4b0bf9faf
SamUsachev <semenusachev10@gmail.com>
2022-12-21 22:50:56 +0300

    first commit

---
+++ src/components/ShoppingCard.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { defaultItems } from '../data';
+import { ItemsTable } from './ItemsTable';
+
+export const ShoppingCard = () => {
+  const [items, setItems] = useState(defaultItems);
+  const result = items.reduce(
+    (perviousValue, currentItem) =>
+      perviousValue + currentItem.count * currentItem.price,
+    0
+  );
+  const Footer = (
+    <div className="result-panel">
+      <span>
+        Общая стоимость: <span className="value">{result}</span> $.
+      </span>
+      <button>Оформить</button>
+    </div>
+  );
+  const EmptyTemplate = (
+    <div className="empty-text">У вас нет товаров в корзине</div>
+  );
+  const handleRemoveItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+  const handleIncreaceCount = (id) => {
+    setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          item.count++;
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleDecreaceCount = (id, count) => {
+    if (count < 2) {
+      handleRemoveItem(id);
+    } else {
+      setItems(
+        items.map((item) => {
+          if (item.id === id) {
+            item.count--;
+          }
+          return item;
+        })
+      );
+    }
+  };
+  return (
+    <div>
+      <h1>Корзина</h1>
+      {!!items.length ? (
+        <ItemsTable
+          items={items}
+          removeItem={handleRemoveItem}
+          increaceCount={handleIncreaceCount}
+          decreaceCount={handleDecreaceCount}
+        />
+      ) : (
+        EmptyTemplate
+      )}
+
+      {!!items.length && Footer}
+    </div>
+  );
+};
